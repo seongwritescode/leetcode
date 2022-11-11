601ef2e54c0888c6eef81b1fd44a97069ef0f22d
seongwritescode <seongwritescode@gmail.com>
2022-11-11 17:34:05 +0700

    update problem 35 and 58

--- leetcode/unit_test/unittest_problem_28.py
+++ leetcode/unit_test/unittest_problem_28.py
@@ -4,9 +4,13 @@ from problem_28 import Solution
 class SearchInsertTest(unittest.TestCase):
 	solution = Solution()
 	def test_insert_test(self):
-		self.assertEqual(2, solution.searchInsert(nums=[1,3,5,6],target=5))
-		self.assertEqual(1, solution.searchInsert(nums = [1,3,5,6], target = 2))
-		self.assertEqual(4, solution.searchInsert(nums = [1,3,5,6], target = 7))
-
+		self.assertEqual(0, self.solution.searchInsert(nums=[1],target=1))
+		self.assertEqual(2, self.solution.searchInsert(nums=[1,3,5,6],target=5))
+		self.assertEqual(1, self.solution.searchInsert(nums = [1,3,5,6], target = 2))
+		self.assertEqual(4, self.solution.searchInsert(nums = [1,3,5,6], target = 7))
+		self.assertEqual(2, self.solution.searchInsert(nums = [1,4,6,7,8,9], target = 6))
+if __name__ == '__main__':
+    # begin the unittest.main()
+    unittest.main()
 		
 
